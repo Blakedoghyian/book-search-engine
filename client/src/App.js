a4bfbdd8c8dfb2799f5160433e14d3e9afe19529
Blakedoghyian <91994720+Blakedoghyian@users.noreply.github.com>
2022-03-21 15:07:30 -0500

    mutations and queries set up, updated searchbooks

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,35 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+//establish a new link to the GraphQL server at its /graphql endpoint with createHttpLink()
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+//instantiate the Apollo Client instance and create the connection to the API endpoint
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +42,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
